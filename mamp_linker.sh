0100ebd5bd40ea656567a51d91c082606af0d34f
Eugene Fedorych <efedoryc@e3r3p6.unit.ua>
2017-08-27 09:52:43 +0300

    Fixed mamp_linker.sh

--- mamp_linker.sh
+++ mamp_linker.sh
@@ -15,8 +15,8 @@ if [ -L $link_path ]; then
 	fi
 fi
 
-printf "\e[33m## Creating link from $mamp_target/$mamp_folder_name to ~/Library/.Containers/MAMP...\e[0m\n"
-if ln -s $mamp_target/$mamp_folder_name ~/Library/.Containers/MAMP; then
+printf "\e[33m## Creating link from $mamp_target/$mamp_folder_name to ~/Library/Containers/MAMP...\e[0m\n"
+if ln -s $mamp_target/$mamp_folder_name ~/Library/Containers/MAMP; then
 	printf "\e[32m## Link created\e[0m\n"
 else
 	printf "\e[31m## Link not created\e[0m\n"
